0ab161e461dd327f790aa3e24f428bf52eee63fc
Suraj2123 <yvdatta@gmail.com>
2026-01-26 22:50:12 +0000

    update py

--- src/mrs/serving/api.py
+++ src/mrs/serving/api.py
@@ -81,9 +81,12 @@ def similar_items(
 
 
 def main() -> None:
+    import os
     import uvicorn
 
-    uvicorn.run("mrs.serving.api:app", host="0.0.0.0", port=8000, reload=False)
+    port = int(os.environ.get("PORT", "8000"))
+    uvicorn.run("mrs.serving.api:app", host="0.0.0.0", port=port, reload=False)
+
 
 
 if __name__ == "__main__":
